c61f8495f1e651bbd51c9901e74bbeaa0acd3b35
phtnaiakashk <nitw.akash@gmail.com>
2025-10-15 15:04:39 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import MathLib;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Add: " + MathLib.add(5, 3));
-        System.out.println("Divide: " + MathLib.divide(10, 2));
-    }
-}
