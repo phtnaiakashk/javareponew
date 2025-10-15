3e66be461d7dfe774c83eba85adb1358ab8a73be
phtnaiakashk <nitw.akash@gmail.com>
2025-10-15 15:05:03 -0500

    Delete src\MathLib.java

--- src\MathLib.java
+++
@@ -1,9 +0,0 @@
-import MathLib;
-import MathLib;
-
-public class MathOperations {
-    public static int add(int a, int b) { return a + b; }
-    public static int subtract(int a, int b) { return a - b; }
-    public static int multiply(int a, int b) { return a * b; }
-    public static double divide(int a, int b) { return b != 0 ? (double)a / b : 0.0; }
-}
