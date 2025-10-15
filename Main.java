922e238b1468dcfcccd0f0bac3773e28b66d861a
phtnaiakashk <nitw.akash@gmail.com>
2025-10-15 14:58:33 -0500

    Converted Main.cpp from C++ to Java

---
+++ Main.java
@@ -0,0 +1,8 @@
+import MathLib;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Add: " + MathLib.add(5, 3));
+        System.out.println("Divide: " + MathLib.divide(10, 2));
+    }
+}
