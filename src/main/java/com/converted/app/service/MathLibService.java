5e3f4fd67a2e05e1f6151eb488ab99f08b89f7af
phtnaiakashk <nitw.akash@gmail.com>
2025-10-16 11:56:18 -0500

    Convert include\MathLib.hpp::service

--- src/main/java/com/converted/app/service/MathLibService.java
+++ src/main/java/com/converted/app/service/MathLibService.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MathLibService {
-    public int add(int a, int b) {
-        return a + b;
+    public int multiply(int a, int b) {
+        return a * b;
     }
 
-    public int subtract(int a, int b) {
-        return a - b;
+    public double divide(int a, int b) {
+        if (b == 0) return 0;
+        return a / b;
     }
 
 }
