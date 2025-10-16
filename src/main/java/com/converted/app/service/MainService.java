4b9731b7615807f1c8ff4dc85024456817583806
phtnaiakashk <nitw.akash@gmail.com>
2025-10-16 11:56:04 -0500

    Convert Main.cpp::service

---
+++ src/main/java/com/converted/app/service/MainService.java
@@ -0,0 +1,12 @@
+package com.converted.app.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+    public int main() {
+        // TODO: implement
+        return 0;
+    }
+
+}
