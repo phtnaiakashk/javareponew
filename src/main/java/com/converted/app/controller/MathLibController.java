462c66f044caafdb519c75bca7caed866bfeff97
phtnaiakashk <nitw.akash@gmail.com>
2025-10-16 11:56:13 -0500

    Convert include\MathLib.h::controller

---
+++ src/main/java/com/converted/app/controller/MathLibController.java
@@ -0,0 +1,30 @@
+package com.converted.app.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import com.converted.app.service.MathLibService;
+
+@RestController
+@RequestMapping("/mathlib")
+public class MathLibController {
+    private final MathLibService service;
+
+    public MathLibController(MathLibService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<int> add(@RequestParam int a, @RequestParam int b) {
+        // TODO: map request parameters and call service
+        int result = service.add(a, b);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping("/subtract")
+    public ResponseEntity<int> subtract(@RequestParam int a, @RequestParam int b) {
+        // TODO: map request parameters and call service
+        int result = service.subtract(a, b);
+        return ResponseEntity.ok(result);
+    }
+
+}
